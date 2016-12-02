a152fd4917e9ee22bf60ab026060d4a44f3e5751
amaranter <ronnyamarante@plusoft.com.br>
2016-12-02 17:17:20 -0200

    teste

--- js/teste.js
+++ js/teste.js
@@ -886,20 +886,20 @@ dynaform.filter = function($target, options) {
 // } catch(e) {}
 
 /* debounce */
-function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait); 
-        if (callNow) func.apply(context, args);
-    };
-};
+// function debounce(func, wait, immediate) {
+//     var timeout;
+//     return function() {
+//         var context = this, args = arguments;
+//         var later = function() {
+//             timeout = null;
+//             if (!immediate) func.apply(context, args);
+//         };
+//         var callNow = immediate && !timeout;
+//         clearTimeout(timeout);
+//         timeout = setTimeout(later, wait); 
+//         if (callNow) func.apply(context, args);
+//     };
+// };
 
 /*
  * LocalStorageFactory
