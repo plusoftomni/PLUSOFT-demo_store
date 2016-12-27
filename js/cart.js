37a16a64d698883b581e2d8fac96a9f6c8765ea2
amaranter <ronnyamarante@plusoft.com.br>
2016-12-27 17:00:56 -0200

    fixing context

--- js/cart.js
+++ js/cart.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
   };
 
   /* external actions of embedded */
-  $("#opene").on("click", function(e) { if (window.plusoftOmniChat && window.plusoftOmniChat.loaded == true) { new window.plusoftOmniChat.messageBroadcaster("omnichat.open_conversation", "open", { serialize: true, domains: ["https:" + window.plusoftOmniChat.base], context: context, origin: "internal" }); } });
+  $("#opene").on("click", function(e) { if (window.plusoftOmniChat && window.plusoftOmniChat.loaded == true) { new window.plusoftOmniChat.messageBroadcaster("omnichat.open_conversation", "open", { serialize: true, domains: ["https:" + window.plusoftOmniChat.base], context: document.getElementById("plusoftOmniChatMain").contentWindow, origin: "internal" }); } });
   $("#minimizee").on("click", function(e) {  });
   $("#closee").on("click", function(e) {  });
   $("#datae").on("click", function(e) {  });
